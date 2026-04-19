92c0b5ebaa19c2748d0d983c6a9d632ab4c696b0
tapot <novakche@gmail.com>
2026-04-19 19:55:49 +0300

    fix script load

--- scripts/ui/core/wave_ui.gd
+++ scripts/ui/core/wave_ui.gd
@@ -6,6 +6,6 @@ extends VBoxContainer
 
 
 func set_progress(value: float, max_value: float) -> void:
-	var safe_max_value := max(max_value, 0.000001)
+	var safe_max_value: float = max(max_value, 0.000001)
 	progress_bar.max_value = safe_max_value
 	progress_bar.value = clamp(value, 0.0, safe_max_value)
